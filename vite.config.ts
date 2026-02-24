c1fcad279e6d3dc09f3c750527224cc7dd660787
Dodilanne <vincaudet@gmail.com>
2026-02-24 11:07:31 -0500

    Add missing files to dist dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 /// <reference types="vitest/config" />
+import { copyFile, writeFile } from "node:fs/promises";
 import dts from "unplugin-dts/vite";
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
+import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
   build: {
@@ -10,9 +12,45 @@ export default defineConfig({
       fileName: "lib",
     },
   },
-  plugins: [dts({ bundleTypes: true })],
+  plugins: [
+    dts({
+      bundleTypes: true,
+    }),
+    copyPackageJson(),
+  ],
   test: {
     environment: "happy-dom",
     globals: true,
   },
 });
+
+function copyPackageJson(): PluginOption {
+  let hasGenerated = false;
+  return {
+    name: "copy-package-json",
+    async writeBundle() {
+      if (hasGenerated) {
+        return;
+      }
+      hasGenerated = true;
+      await Promise.all([
+        writeFile(
+          "dist/package.json",
+          JSON.stringify(
+            {
+              ...pkg,
+              files: ["**"],
+              main: "./lib.umd.cjs",
+              module: "./lib.js",
+              types: "./lib.d.ts",
+            },
+            null,
+            2,
+          ),
+        ),
+        copyFile("./README.md", "dist/README.md"),
+        copyFile("./LICENSE", "dist/LICENSE"),
+      ]);
+    },
+  };
+}
