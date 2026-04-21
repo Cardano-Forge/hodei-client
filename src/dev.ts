df3654a30d98e704f37a3316791ba52088a98ff3
Vincent Audet <vincaudet@gmail.com>
2026-04-21 09:08:26 -0400

    Add playground mode with weld integration (#16)

--- src/dev.ts
+++ src/dev.ts
@@ -1,10 +1,29 @@
+import {
+  type AddressHex,
+  createWeldInstance,
+  type EnabledWalletApi as WeldEnabledWalletApi,
+} from "@ada-anvil/weld";
+import { builtinPlugins } from "@ada-anvil/weld/plugins";
+import { hodeiPlugin } from "@ada-anvil/weld-plugin-hodei";
 import { submitTx } from "./anvil";
-import type { EnabledWalletApi, InitialWalletApi } from "./api";
+import type {
+  EnabledWalletApi as HodeiEnabledWalletApi,
+  InitialWalletApi,
+} from "./api";
 import { type Config, DEFAULT_CONFIG } from "./config";
 import { initialize } from "./lib";
 import { assert } from "./utils";
 
-const devConfig: Config = {
+type PlaygroundApi = {
+  mode: "vanilla" | "weld";
+  init: () => void;
+  deinit: () => void;
+  wallet: () => (HodeiEnabledWalletApi | WeldEnabledWalletApi) | undefined;
+  dev: () => DevInitialWalletApi["dev"];
+  connect: () => Promise<void>;
+};
+
+const baseConfig: Config = {
   ...DEFAULT_CONFIG,
   bridge: {
     ...DEFAULT_CONFIG.bridge,
@@ -15,29 +34,88 @@ const devConfig: Config = {
     baseDelay: 60000,
   },
   debug: true,
-  onError: ({ error }) => {
-    updateWalletState({ status: "error", error: error ?? "unknown" });
-  },
-  onClose: ({ code, reason }) => {
-    updateWalletState({ status: "closed", code, reason });
-  },
-  onWalletUpdate: (wallet) => {
-    updateWalletState({ status: "connected", ...wallet });
-  },
   waitForPairing: true,
 };
 
-function updateWalletState(state: unknown) {
+const playgrounds: Record<PlaygroundApi["mode"], () => PlaygroundApi> = {
+  vanilla: (): PlaygroundApi => {
+    let wallet: HodeiEnabledWalletApi | undefined;
+
+    return {
+      mode: "vanilla",
+      init: () => {
+        initialize({
+          ...baseConfig,
+          onError: ({ error }) => {
+            updateWalletState({ status: "error", error: error ?? "unknown" });
+          },
+          onClose: ({ code, reason }) => {
+            updateWalletState({ status: "closed", code, reason });
+          },
+          onWalletUpdate: (wallet) => {
+            updateWalletState({ status: "connected", ...wallet });
+          },
+        });
+        updateWalletState({ status: "disconnected" });
+      },
+      deinit: () => {},
+      dev: () => (window.cardano?.hodei as DevInitialWalletApi)?.dev,
+      wallet: () => wallet,
+      connect: async () => {
+        try {
+          console.log("connecting");
+          wallet = await window.cardano?.hodei?.enable();
+          console.log("connected and paired!");
+        } catch (error) {
+          console.log("failed to connect:", error);
+        }
+      },
+    };
+  },
+  weld: (): PlaygroundApi => {
+    const weld = createWeldInstance({
+      debug: true,
+      plugins: [
+        ...builtinPlugins,
+        hodeiPlugin({
+          ...baseConfig,
+          initialize,
+        }),
+      ],
+    });
+
+    return {
+      mode: "weld",
+      init: () => {
+        weld.init();
+        weld.wallet.subscribe(updateWalletState);
+        updateWalletState(weld.wallet.getState());
+      },
+      deinit: () => weld.cleanup(),
+      dev: () => (window.cardano?.hodei as DevInitialWalletApi)?.dev,
+      wallet: () => weld.wallet.handler?.enabledApi,
+      connect: async () => {
+        await weld.wallet.connectAsync("hodei");
+      },
+    };
+  },
+} satisfies Record<string, () => PlaygroundApi>;
+
+let playground = playgrounds.vanilla();
+playground.init();
+
+function updateWalletState(state: object) {
   const el = document.querySelector("#walletState");
   assert(el);
-  el.innerHTML = JSON.stringify(state, null, 2);
+  el.innerHTML = JSON.stringify(
+    {
+      mode: playground.mode,
+      ...state,
+    },
+    null,
+    2,
+  );
 }
-
-initialize(devConfig);
-
-let wallet: EnabledWalletApi | undefined;
-
-connect();
 
 // Kbd shortcuts
 window.onkeydown = (event) => {
@@ -61,6 +139,8 @@ window.onkeydown = (event) => {
     fn = unlink;
   } else if (event.key === "B") {
     fn = debug;
+  } else if (event.key === "M") {
+    fn = toggleMode;
   } else if (event.key === "K") {
     fn = clearConsole;
   }
@@ -86,32 +166,39 @@ document.querySelector("#hang")?.addEventListener("click", hang);
 document.querySelector("#debug")?.addEventListener("click", debug);
 document.querySelector("#disconnect")?.addEventListener("click", disconnect);
 document.querySelector("#unlink")?.addEventListener("click", unlink);
+document.querySelector("#unlink")?.addEventListener("click", unlink);
+document.querySelector("#toggleMode")?.addEventListener("click", toggleMode);
 
 // Functions
+
+function toggleMode() {
+  playground.deinit();
+  if (playground.mode === "vanilla") {
+    playground = playgrounds.weld();
+  } else {
+    playground = playgrounds.vanilla();
+  }
+  playground.init();
+}
+
 function hang() {
-  getDevApi()?.hang();
+  playground.dev()?.hang();
 }
 
 function debug() {
-  getDevApi()?.debug();
+  playground.dev()?.debug();
 }
 
 async function connect() {
-  try {
-    console.log("connecting");
-    wallet = await window.cardano?.hodei?.enable();
-    console.log("connected and paired!");
-  } catch (error) {
-    console.log("failed to connect:", error);
-  }
+  playground.connect();
 }
 
 async function disconnect() {
-  getDevApi()?.disconnect();
+  playground.dev()?.disconnect();
 }
 
 async function unlink() {
-  getDevApi()?.unlink();
+  playground.dev()?.unlink();
 }
 
 async function clearConsole() {
@@ -120,16 +207,15 @@ async function clearConsole() {
 
 async function signDataStake() {
   try {
-    if (!wallet) {
-      throw new Error("wallet not connected");
-    }
+    const wallet = playground.wallet();
+    assert(wallet, "wallet not connected");
     const data = utf8ToHex("hello world!");
     console.log("data", data);
     const [rewardAddress] = await wallet.getRewardAddresses();
     if (!rewardAddress) {
       throw new Error("no reward address");
     }
-    const signRes = await wallet.signData(rewardAddress, data);
+    const signRes = await wallet.signData(rewardAddress as AddressHex, data);
     console.log("signRes", signRes);
   } catch (error) {
     console.error(error);
@@ -146,13 +232,12 @@ function utf8ToHex(input: string) {
 
 async function signDataPayment() {
   try {
-    if (!wallet) {
-      throw new Error("wallet not connected");
-    }
+    const wallet = playground.wallet();
+    assert(wallet, "wallet not connected");
     const data = utf8ToHex("hello world!");
     console.log("data", data);
     const changeAddress = await wallet.getChangeAddress();
-    const signRes = await wallet.signData(changeAddress, data);
+    const signRes = await wallet.signData(changeAddress as AddressHex, data);
     console.log("signRes", signRes);
   } catch (error) {
     console.error(error);
@@ -161,15 +246,14 @@ async function signDataPayment() {
 
 async function signTx() {
   try {
-    if (!wallet) {
-      throw new Error("wallet not connected");
-    }
+    const wallet = playground.wallet();
+    assert(wallet, "wallet not connected");
 
     const networkId = await wallet.getNetworkId();
     const network = networkId === 1 ? "mainnet" : "preprod";
 
-    const { baseUrl, apiKey } = devConfig.anvil[network];
-    const url = new URL(`${baseUrl}/transactions/build`);
+    const { baseUrl, apiKey } = baseConfig.anvil[network];
+    const url = new URL(`${baseUrl} /transactions/build`);
     const changeAddress = await wallet.getChangeAddress();
     const utxos = await wallet.getUtxos();
     console.log("utxos", utxos);
@@ -188,7 +272,7 @@ async function signTx() {
 
     if (!buildRes.ok) {
       throw new Error(
-        `build failed: ${buildRes.status} ${buildRes.statusText}`,
+        `build failed: ${buildRes.status} ${buildRes.statusText} `,
       );
     }
 
@@ -204,7 +288,7 @@ async function signTx() {
 
     if (confirm("submit?")) {
       const submitRes = await submitTx({
-        config: devConfig,
+        config: baseConfig,
         network: (await wallet.getNetworkId()) === 1 ? "mainnet" : "preprod",
         transaction: parsed.complete,
         signature: signRes,
@@ -218,15 +302,14 @@ async function signTx() {
 
 async function delegate() {
   try {
-    if (!wallet) {
-      throw new Error("wallet not connected");
-    }
+    const wallet = playground.wallet();
+    assert(wallet, "wallet not connected");
 
     const networkId = await wallet.getNetworkId();
     const network = networkId === 1 ? "mainnet" : "preprod";
 
-    const { baseUrl, apiKey } = devConfig.anvil[network];
-    const url = new URL(`${baseUrl}/transactions/build`);
+    const { baseUrl, apiKey } = baseConfig.anvil[network];
+    const url = new URL(`${baseUrl} /transactions/build`);
     const changeAddress = await wallet.getChangeAddress();
     const utxos = await wallet.getUtxos();
     const buildRes = await fetch(url, {
@@ -259,7 +342,7 @@ async function delegate() {
 
     if (!buildRes.ok) {
       throw new Error(
-        `build failed: ${buildRes.status} ${buildRes.statusText}`,
+        `build failed: ${buildRes.status} ${buildRes.statusText} `,
       );
     }
 
@@ -275,7 +358,7 @@ async function delegate() {
 
     if (confirm("submit?")) {
       const submitRes = await submitTx({
-        config: devConfig,
+        config: baseConfig,
         network: (await wallet.getNetworkId()) === 1 ? "mainnet" : "preprod",
         transaction: parsed.complete,
         signature: signRes,
@@ -295,7 +378,3 @@ export type DevInitialWalletApi = InitialWalletApi & {
     disconnect(): void;
   };
 };
-
-function getDevApi(): DevInitialWalletApi["dev"] {
-  return (window.cardano?.hodei as DevInitialWalletApi)?.dev;
-}
